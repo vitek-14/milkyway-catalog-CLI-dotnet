f866c8c04c1dfe54c2081454d9206a980f175bfa
Vít Šilhavý <vitekkt@gmail.com>
2026-05-06 20:16:29 +0200

    base entity added

---
+++ SpaceCatalog/SpaceCatalog.Domain/Base/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceCatalog.Domain.Base
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
