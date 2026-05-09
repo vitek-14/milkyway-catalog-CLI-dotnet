65a6e2acd86324c9fbfa4a97878577e22cd93556
Vít Šilhavý <vitekkt@gmail.com>
2026-05-09 16:39:29 +0200

    EntityBase set as abstract class

--- SpaceCatalog/SpaceCatalog.Domain/Base/EntityBase.cs
+++ SpaceCatalog/SpaceCatalog.Domain/Base/EntityBase.cs
@@ -1,6 +1,6 @@
 namespace SpaceCatalog.Domain.Base
 {
-    public class EntityBase
+    abstract public class EntityBase
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
