a2d2c3a82c5e5504ca8ca05c2c8f144998f90420
Vít Šilhavý <vitekkt@gmail.com>
2026-05-09 10:20:52 +0200

    entity base update

--- SpaceCatalog/SpaceCatalog.Domain/Base/EntityBase.cs
+++ SpaceCatalog/SpaceCatalog.Domain/Base/EntityBase.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace SpaceCatalog.Domain.Base
 {
     public class EntityBase
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
     }
 }
